1d6e8b681fe54a114e6fd99e0815675626f5ee7d
Disha Sheth <dsheth18@gmail.com>
2016-11-10 23:36:10 -0800

    Add files via upload

---
+++ maria.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class maria here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class maria extends Actor
+{
+    /**
+     * Act - do whatever the maria wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+     public maria() 
+    { 
+        GreenfootImage image = getImage(); 
+         image.scale(200,350); 
+     }
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
