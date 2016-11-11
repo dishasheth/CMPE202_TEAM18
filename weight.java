bf0d01e816dfb2fbfba8f116c5171909992dbee0
Disha Sheth <dsheth18@gmail.com>
2016-11-10 23:36:30 -0800

    Add files via upload

---
+++ weight.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class weight here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class weight extends Actor
+{
+    /**
+     * Act - do whatever the weight wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+     public weight() 
+    { 
+         GreenfootImage image = getImage(); 
+         image.scale(50,50); 
+     }
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
