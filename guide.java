49782dbd0676fee1500167bff5c82fdca2058704
Manu Barsainyan <mbarsainyan210692@gmail.com>
2016-11-10 18:42:52 -0800

    Add files via upload

---
+++ guide.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class guide here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class guide extends Actor
+{
+    /**
+     * Act - do whatever the guide wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public guide()
+    {
+    GreenfootImage image = getImage() ;
+        image.scale( 150, 180 ) ; 
+    }
+    public void act() 
+    {
+       
+        // Add your action code here.
+    }    
+}
