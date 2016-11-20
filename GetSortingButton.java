d3fb72eddb0618161491358a477e690b29d3ac56
Ami Patel <ami.patel317@gmail.com>
2016-11-20 15:16:31 -0800

    Add files via upload

---
+++ GetSortingButton.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GetSorting here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GetSortingButton extends Button
+{
+    public GetSortingButton()
+    {
+    GreenfootImage image = getImage() ;
+        image.scale( 600, 80 ) ;
+    }
+    /**
+     * Act - do whatever the GetSorting wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
